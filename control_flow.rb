c364e99c3cb50f0ad08d59105e8d8b6bb2b1f8dc
Khanh Tran <katetrannbkhanh@gmail.com>
2023-12-31 00:12:46 -0800

    commit tree d0891b5ffff29efabeb11c49f6cb7526b810f0e8

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,19 @@
 def admin_login(username, password)
-  # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    "Access granted"
+  else
+    "Access denied"
+  end
 end
 
-def hows_the_weather(temperature)
-  # your code here
+def hows_the_weather(temp)
+  if temp < 40
+    "It's brisk out there!"
+  elsif temp >= 40 && temp <= 65
+    "It's a little chilly out there!"
+  elsif temp > 85 
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end 
 end
-
-def fizzbuzz(num)
-  # your code here
-end
-
-def calculator(operation, num1, num2)
-  # your code here
-end
-
